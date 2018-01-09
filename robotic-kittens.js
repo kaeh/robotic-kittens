83eb88a7d8f9bdac24e25ee5353b8cdc9d9a223f
kaeh <kaeh@users.noreply.github.com>
2018-01-09 17:36:43 +0100

    Add passive AI with automation.automatize key.

--- robotic-kittens.js
+++ robotic-kittens.js
@@ -15,38 +15,60 @@ craftAll = function(resourceName, craftName) {
 
 function automation_init() {
 	window.automation = {
+		automatize: {
+			hunt: true,
+			parchment: true,
+			manuscript: false,
+			compedium: false,
+			blueprint: false,
+
+			wood: true,
+			beam: true,
+			slab: true,
+			steel: true,
+			plate: true,
+
+			promotion: true,
+
+			praise: true,
+
+			observe: true
+		},
+
 		sendHunters: function() {
 			var catpower = getResource('manpower');
-		 	if (isAlmostFilled(catpower)) {
+		 	if (automation.automatize.hunt && isAlmostFilled(catpower)) {
 				violetLog('Sending hunters.');
 			 	$("a:contains('Send hunters')").click();
-			 	if (gamePage.workshop.getCraft('parchment').unlocked) {gamePage.craftAll('parchment'); }
-			 	if (gamePage.workshop.getCraft('manuscript').unlocked) { gamePage.craftAll('manuscript'); }
-			 	if (gamePage.workshop.getCraft('compedium').unlocked) { gamePage.craftAll('compedium'); }
-			 	// if (gamePage.workshop.getCraft('blueprint').unlocked) { gamePage.craftAll('blueprint'); }
+			 	if (gamePage.workshop.getCraft('parchment').unlocked && automation.automatize.parchment) {gamePage.craftAll('parchment'); }
+			 	if (gamePage.workshop.getCraft('manuscript').unlocked && automation.automatize.manuscript) { gamePage.craftAll('manuscript'); }
+			 	if (gamePage.workshop.getCraft('compedium').unlocked && automation.automatize.compedium) { gamePage.craftAll('compedium'); }
+			 	if (gamePage.workshop.getCraft('blueprint').unlocked && automation.automatize.blueprint) { gamePage.craftAll('blueprint'); }
 		 	}
 		},
 		minMaxCraft: function() {
 			// Wood craft
-			var catnip = getResource('catnip');
-			// Do not refine if production is negative or it's winter or it's past mid autumn
-			var shooldRefine = !(catnip.perTickCached < 0 || gamePage.calendar.getCurSeason().name === 'winter' || (gamePage.calendar.getCurSeason().name === 'autumn' && gamePage.calendar.day > 50));
-			if (shooldRefine && isAlmostFilled(catnip)) {
-				violetLog('Crafting wood');
-				gamePage.craftAll('wood');
+			if (automation.automatize.wood) {
+				var catnip = getResource('catnip');
+				// Do not refine if production is negative or it's winter or it's past mid autumn
+				var shooldRefine = !(catnip.perTickCached < 0 || gamePage.calendar.getCurSeason().name === 'winter' || (gamePage.calendar.getCurSeason().name === 'autumn' && gamePage.calendar.day > 50));
+				if (shooldRefine && isAlmostFilled(catnip)) {
+					violetLog('Crafting wood');
+					gamePage.craftAll('wood');
+				}
 			}
 			// Beam craft
-			craftAll('wood', 'beam');
+			if (automation.automatize.beam) { craftAll('wood', 'beam'); }
 			// Slab craft
-			craftAll('minerals', 'slab');
+			if (automation.automatize.slab) { craftAll('minerals', 'slab'); }
 			// Steel & Plate craft
 			var coal = getResource('coal');
 			var iron = getResource('iron');
-			if (isAlmostFilled(coal)) {
+			if (automation.automatize.steel && isAlmostFilled(coal)) {
 				violetLog('Crafting steel');
 				gamePage.craftAll('steel');
 			}
-			else if (isAlmostFilled(iron)) {
+			else if (automation.automatize.steel && automation.automatize.plate && isAlmostFilled(iron)) {
 				violetLog('Crafting steel and plate');
 				gamePage.craftAll('steel');
 				gamePage.craftAll('plate');
@@ -54,6 +76,8 @@ function automation_init() {
 
 		},
 		minMaxPromotion: function() {
+			if (!automation.automatize.promotion) { return; }
+
 			var gold = getResource('gold');
 			if (isAlmostFilled(gold)) {
 				violetLog('Promoting kittens');
@@ -61,6 +85,8 @@ function automation_init() {
 			}
 		},
 		minMaxFaith: function() {
+			if (!automation.automatize.praise) { return; }
+
 			var faith = getResource('faith');
 			if (isAlmostFilled(faith)) {
 				violetLog('PRAISE THE SUN !');
@@ -68,6 +94,8 @@ function automation_init() {
 			}
 		},
 		observeSky: function() {
+			if (!automation.automatize.observe) { return; }
+
 			if ($('#observeBtn')[0]) {
 				$('#observeBtn').click();
 				violetLog('Observing the sky.');
